84f9c9867328bf834852e6dd66d436750fd27648
saifudheen9745 <saifu.ofcl@gmail.com>
2023-08-08 15:02:55 +0530

    [update]: updated login service to handle validation and api, implemented toasts for notification

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { LoginServiceService } from './services/login-service.service';
+import { ToastCollectionService } from 'src/app/shared/services/toast/toast-collection.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
+  email: string;
+  password: string;
+  emailError: string;
+  passwordError: string;
+  constructor(private loginService: LoginServiceService, private toasts:ToastCollectionService) {}
 
+  validateEmail = () => {
+    this.loginService.validateEmail(this.email);
+    this.loginService.emailError.subscribe((error) => {
+      this.emailError = error;
+    });
+  };
+
+  validatePassword = () => {
+    this.passwordError = this.loginService.validatePassword(this.password);
+  };
+
+  loginFormSubmit = ()=>{
+    const email = "email@gmail.com"
+    const pass = 'pass'
+    if(this.email !== email || this.password !== pass){
+      this.toasts.errorToast()
+    }else{
+      this.toasts.successToast()
+    }
+  }
 }
