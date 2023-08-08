25b5acdee6ce54094c318a8c80d1bdcb569b88e4
saifudheen9745 <saifu.ofcl@gmail.com>
2023-08-08 10:28:50 +0530

    [update]: updated the router link in signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,16 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/login-page/login-page.module').then(
         (m) => m.LoginPageModule
-      ),title:'Swarm | Login',
+      ),
+    title: 'Swarm | Login',
+  },
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./pages/signup-page/signup-page.module').then(
+        (m) => m.SignupPageModule
+      ),
+    title: 'Swarm | SignUp',
   },
 ];
 
